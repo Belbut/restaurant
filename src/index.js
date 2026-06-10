f9247f8dda03a7ce1594cc1124f289ad48d75471
Belbut <80793911+Belbut@users.noreply.github.com>
2026-06-10 20:48:41 +0100

    feat: add tab event on click

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import homeContent from "./tabs/home-tab.js"
 import menuContent from "./tabs/menu-tab.js";
-import contactContent from "./tabs/contact-tab.js";
+import aboutContent from "./tabs/contact-tab.js";
 
 console.log("hi world");
 console.log("bye")
 
 homeContent()
-menuContent()
-contactContent()
+
+const homeBtn = document.querySelector("#home")
+const menuBtn = document.querySelector("#menu")
+const aboutBtn = document.querySelector("#about")
+
+const addTabSwitchEvent = (btn, tabContent) => {
+    const contentDiv = document.querySelector("#content")
+   btn.addEventListener("click", ()=> {
+    contentDiv.textContent = "";
+
+    tabContent();
+    console.log("123")
+  })
+}
+
+  addTabSwitchEvent(homeBtn, homeContent)
+  addTabSwitchEvent(menuBtn, menuContent)
+  addTabSwitchEvent(aboutBtn, aboutContent)
+
